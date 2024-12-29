ddcbf1d41948d36db5542d100075928abcdf2bbc
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-12-29 18:56:59 +0100

    refactor: Update Dashboard component
    • Introduced the BalanceBox and RightSideBar components to display user information and recent transactions.
    • Added a HeaderBox component to display a greeting and account information.
    • Updated the component's styles to use a new layout and added a home class.
    • Removed the original page div and replaced it with a new section layout.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,36 @@
+import BalanceBox from '@/components/BalanceBox';
+import RightSideBar from '@/components/RightSideBar';
+
+import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Dashboard = () => {
-  return (
-    <div>page</div>
-  )
+    const loggedIn = {firstName:'Vladyslav', lastName: 'Olshevskyi', email: 'olshevkyi.work@gmail.com'};
+    return (
+        <section className="home">
+        <div className="home-content">
+          <header className="home-header">
+                <HeaderBox
+                type='greeting'
+                title='Welcome'
+                user={loggedIn?.firstName || 'Guest'}
+                subtext='Manage your account and transactions'
+                />
+                <BalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1337.24}
+                />
+            </header>
+            RECENT
+        </div>
+        <RightSideBar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{},{}]}
+        />
+    </section>
+    )
 }
 
 export default Dashboard

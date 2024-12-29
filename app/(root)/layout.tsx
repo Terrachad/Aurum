7fd3a350b765f1f1f18c62350214b90069840230
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2024-12-29 18:55:54 +0100

    chore: Update RootLayout component
    Added user information to the RootLayout component.
    Introduced the SideBar component and passed the user object as a prop.
    Added a logo image and a MobileNav component to the layout.
    Updated the component's styles to use a flexbox layout and added a root-layout class.
    This change enhances the overall user experience by adding user-specific information to the layout.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,32 @@
+import MobileNav from "@/components/MobileNav";
+import SideBar from "@/components/SideBar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstname: 'VLADY', lastname: 'olshevskyi'}
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <SideBar user={loggedIn}/>
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+              <Image
+              src='/icons/aurum-logo.svg'
+              width={100}
+              height={30}
+              alt="menu icon"
+              className="ml-[-10]"
+              />
+              <div>
+                <MobileNav user={loggedIn}/>
+              </div>
+          </div>
+          {children}
+        </div>
+
     </main>
   );
 }

80146ed78ba53702292f83bd2a663168e11ddffd
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2025-01-06 20:42:14 +0100

    add: added Appwrite client creation
    • Added imports for Appwrite client creation and cookie handling
    
    • Implemented logic for creating an Appwrite admin client and session client
    
    • Updated signIn function to create a new user account and generate a session cookie
    
    • Added getLoggedInUser function to retrieve the logged-in user's information
    
    • Improved code organization and readability

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,10 @@
 'use server'
 
+import { ID } from "node-appwrite"
+import { createAdminClient, createSessionClient } from "../appwrite"
+import { cookies } from "next/headers"
+import { parseStringify } from "../utils"
+
 
 export const signIn = async () => {
     try {
@@ -11,8 +16,32 @@ export const signIn = async () => {
 
 export const signUp = async (userData: SignUpParams) => {
     try {
+        const { account } = await createAdminClient();
+        const {email, password, firstName, lastName} = userData;
+        const newUserAcc = await account.create(ID.unique(), email, password, `${firstName} ${lastName}`);
+        const session = await account.createEmailPasswordSession(email, password);
+        const cookieStore = await cookies();
         
+        cookieStore.set("aurum-appwrite-session", session.secret, {
+          path: "/",
+          httpOnly: true,
+          sameSite: "strict",
+          secure: true,
+        });
+        return parseStringify(newUserAcc)
     } catch (error) {
         console.log(error)
     }
 }
+
+
+
+export async function getLoggedInUser() {
+    try {
+        const { account } = await createSessionClient();
+        return await account.get();
+    } catch (error) {
+        console.log(error)
+    }
+}
+  

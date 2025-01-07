12511b1f7a3991efcfbe580c52253b475e94b3fc
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2025-01-07 21:20:12 +0100

    refactor: Updated signIn and signUp functions, added logoutAccount function, and improved code organization.
    • Updated the signIn function to accept email and password as parameters and return the parsed response.
    
    • Updated the signUp function to accept userData as a parameter and return the parsed response.
    
    • Added a new logoutAccount function to delete the Appwrite session and cookie.
    
    • Updated the getLoggedInUser function to parse the response and log it to the console.
    
    • Improved code organization and removed unused variables.
    
    • Made some minor styling adjustments to the code.

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,9 +6,11 @@ import { cookies } from "next/headers"
 import { parseStringify } from "../utils"
 
 
-export const signIn = async () => {
+export const signIn = async ({email, password} : signInProps) => {
     try {
-        
+        const { account } = await createAdminClient();
+        const response = await account.createEmailPasswordSession(email, password)
+        return parseStringify(response)
     } catch (error) {
         console.log(error)
     }
@@ -39,9 +41,20 @@ export const signUp = async (userData: SignUpParams) => {
 export async function getLoggedInUser() {
     try {
         const { account } = await createSessionClient();
-        return await account.get();
+        const resUser = await account.get();
+        console.log({resUser})
+        return parseStringify(resUser)
     } catch (error) {
         console.log(error)
     }
 }
   
+export const logoutAccount = async () => {
+    try {
+        const {account} = await createSessionClient();
+        (await cookies()).delete("aurum-appwrite-session")
+        await account.deleteSession("current")
+    } catch (error) {
+        return error;
+    }
+}

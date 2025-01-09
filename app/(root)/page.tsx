cf399dc967bbcfe4ad353a73d68a7dacb3fc7543
Vladyslav Olshevkyi <94255879+Terrachad@users.noreply.github.com>
2025-01-09 23:27:04 +0100

    refactor: Use getLoggedInUser to fetch logged in user and redirect to sign-in page if not logged in
    • Imported getLoggedInUser from user.actions and used it to fetch the logged in user
    
    • Added a check to see if the user is logged in, and if not, redirect to the sign-in page
    
    • Updated the HeaderBox component to display the logged in user's name instead of first name
    
    • Added a mask property to the banks array in the RightSideBar component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,14 @@ import BalanceBox from '@/components/BalanceBox';
 import RightSideBar from '@/components/RightSideBar';
 
 import HeaderBox from '@/components/ui/HeaderBox'
+import { getLoggedInUser } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const Dashboard = () => {
-    const loggedIn = {firstName:'Vladyslav', lastName: 'Olshevskyi', email: 'olshevkyi.work@gmail.com'};
+const Dashboard = async () => {
+    const loggedIn = await getLoggedInUser();
+  
+    if(!loggedIn) redirect('/sign-in')
     return (
         <section className="home">
         <div className="home-content">
@@ -13,7 +17,7 @@ const Dashboard = () => {
                 <HeaderBox
                 type='greeting'
                 title='Welcome'
-                user={loggedIn?.firstName || 'Guest'}
+                user={loggedIn?.name || 'Guest'}
                 subtext='Manage your account and transactions'
                 />
                 <BalanceBox
@@ -27,7 +31,7 @@ const Dashboard = () => {
         <RightSideBar 
         user={loggedIn}
         transactions={[]}
-        banks={[{ currentBalance: 420},{}]}
+        banks={[{ currentBalance: 420},{mask: 1234}]}
         />
     </section>
     )
